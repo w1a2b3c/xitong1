004eb1d561f1b20cc50b7cf99b7744a41651df61
魏涛 <127228647+w1a2b3c@users.noreply.github.com>
2024-11-13 10:06:12 +0800

    Add files via upload

---
+++ misc.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * @copyright   Leyun internet Technology(Shanghai)Co.,Ltd
+ * @license     http://www.dzzoffice.com/licenses/license.txt
+ * @package     DzzOffice
+ * @link        http://www.dzzoffice.com
+ * @author      zyx(zyx@dzz.cc)
+ */
+define('APPTYPEID', 100);
+
+define('CURSCRIPT', 'misc');
+define('DZZSCRIPT', 'index.php');
+define('BASESCRIPT', basename(__FILE__));
+require './core/dzzstart.php';
